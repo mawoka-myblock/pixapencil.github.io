68b8ae60162dd82de07e5ca319c34bd287497889
therealbluepandabear <justaguyoney@gmail.com>
2022-07-01 16:03:43 +1200

    Added 'Get it on F-Droid' and 'Get it on GitHub' buttons.

---
+++ main.js
@@ -0,0 +1,10 @@
+let fdroid_btn = document.getElementById("f-droid_btn")
+let github_btn = document.getElementById("github_btn")
+
+fdroid_btn.addEventListener("click", function() {
+    location.href = "https://f-droid.org/en/packages/com.therealbluepandabear.pixapencil/"
+})
+
+github_btn.addEventListener("click", function() {
+    location.href = "https://github.com/therealbluepandabear/PixaPencil/releases/latest"
+})
