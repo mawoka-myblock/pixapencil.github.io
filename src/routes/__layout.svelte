c3dfb715e60038c13c09092b5b7187a8172d70af
Mawoka <mawoka-myblock@e.email>
2022-07-03 17:37:30 +0200

    Website in Svelte(Kit)

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+	import Navbar from '../lib/navbar.svelte';
+</script>
+
+<div class="min-h-screen min-w-full bg-gradient-to-b from-[#00b09b] via-[#50be6f] to-[#83c653]">
+	<Navbar />
+	<slot />
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<div class="pt-16">
+	<div class="text-center mt-20">
+		<h1 class="text-7xl">PixaPencil</h1>
+		<p>The #1, open source pixel art editor for Android</p>
+	</div>
+
+	<div class="flex justify-center mt-8">
+		<div class="grid grid-cols-2 gap-2">
+			<a
+				href="https://github.com/therealbluepandabear/PixaPencil/releases/latest"
+				target="_blank"
+				class="p-2 border-lime-300 border-2 transition hover:bg-lime-300 text-center rounded text-lg"
+				>Get it on GitHub
+			</a>
+			<a
+				href="https://f-droid.org/en/packages/com.therealbluepandabear.pixapencil/"
+				target="_blank"
+				class="p-2 border-lime-300 border-2 transition hover:bg-lime-300 text-center rounded text-lg"
+				>Get it on F-Droid</a
+			>
+		</div>
+	</div>
+</div>
